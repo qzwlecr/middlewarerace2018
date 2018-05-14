0a2a388b853d9a7ca485bab64b72a5f3e6717a77
qzwlecr <qzwlecr@gmail.com>
2018-05-14 13:48:51 +0800

    Fix error handling.

--- src/discovery/provider.go
+++ src/discovery/provider.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"encoding/json"
 	"context"
-	"errors"
 )
 
 func NewProvider(name string, info ProviderInfo, endpoints []string) *Provider {
@@ -33,26 +32,21 @@ func NewProvider(name string, info ProviderInfo, endpoints []string) *Provider {
 
 }
 
-func (p *Provider) Start() error {
-
-	ch, err := p.keepAlive()
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
+func (p *Provider) Start() {
+	ch := p.keepAlive()
 
 	for {
 		select {
-		case err := <-p.stop:
+		case <-p.stop:
 			p.revoke()
-			return err
+			return
 		case <-p.client.Ctx().Done():
-			return errors.New("provider closed")
+			return
 		case _, ok := <-ch:
 			if !ok {
 				log.Println("KA channel closed")
 				p.revoke()
-				return nil
+				return
 			}
 		}
 	}
@@ -62,35 +56,39 @@ func (p *Provider) Stop() {
 	p.stop <- nil
 }
 
-func (p *Provider) keepAlive() (<-chan *clientv3.LeaseKeepAliveResponse, error) {
+func (p *Provider) keepAlive() <-chan *clientv3.LeaseKeepAliveResponse {
 
 	info := &p.info
 
-	key := "services/" + p.name
+	key := p.name
 	value, _ := json.Marshal(info)
 
-	resp, err := p.client.Grant(context.TODO(), MinTTL)
+	resp, err := p.client.Grant(context.Background(), MinTTL)
 	if err != nil {
 		log.Fatal(err)
-		return nil, err
+		return nil
 	}
 
-	_, err = p.client.Put(context.TODO(), key, string(value), clientv3.WithLease(resp.ID))
+	_, err = p.client.Put(context.Background(), key, string(value), clientv3.WithLease(resp.ID))
 	if err != nil {
 		log.Fatal(err)
-		return nil, err
+		return nil
 	}
 	p.leaseId = resp.ID
 
-	return p.client.KeepAlive(context.TODO(), resp.ID)
+	ret, err := p.client.KeepAlive(context.Background(), resp.ID)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	return ret
 }
 
 func (p *Provider) revoke() error {
 
-	_, err := p.client.Revoke(context.TODO(), p.leaseId)
+	_, err := p.client.Revoke(context.Background(), p.leaseId)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Provider:%s stop\n", p.name)
 	return err
 }
