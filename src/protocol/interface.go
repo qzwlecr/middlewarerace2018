2b9d7473c4c367be5d9603fdecddef349b3114ad
klx3300 <inc19991@gmail.com>
2018-05-17 13:57:50 +0800

    oop-ify.

--- src/protocol/interface.go
+++ src/protocol/interface.go
@@ -1,26 +1,25 @@
 package protocol
 
+// Protocol : make go happy
+type Protocol interface {
+	toByteArr() (buffer []byte)
+	fromByteArr(buffer []byte)
+}
+
+// Converter : make go happy
 type Converter interface {
-	HTTPToCustom(buf []byte) (req Request)
-	CustomToDubbo(buf []byte) (buf2 []byte)
-	DubboToCustom(buf []byte) (buf2 []byte)
-	CustomToHTTP(buf []byte) (rep Reply)
+	HTTPToCustom(httpreq Protocol) (req Protocol)
+	CustomToDubbo(custreq Protocol) (dubboreq Protocol)
+	DubboToCustom(dubboresp Protocol) (custresp Protocol)
+	CustomToHTTP(resp Protocol) (httpresp Protocol)
 }
 
-type Request struct {
-	outBuf   []byte
-	someInfo string
-}
-
-type Reply struct {
-	outBuf   []byte
-	someInfo string
-}
-
+// CustRequest : make go happy
 type CustRequest struct {
 	content []byte
 }
 
+// CustResponse : make go happy
 type CustResponse struct {
 	delay uint32
 	reply []byte
