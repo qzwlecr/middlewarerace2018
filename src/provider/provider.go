9f3aa7c2f6f1c3ebae73be44f057c87ef4dad7d9
Kyler KY <kylerky@live.com>
2018-05-27 20:23:48 +0800

    name those anonymous functions in handleReq

--- src/provider/provider.go
+++ src/provider/provider.go
@@ -87,6 +87,12 @@ func (p *Provider) Start() {
 	}(ch, tcpCh)
 }
 
+// maintaing timing map
+type tMapEntry struct {
+	id   [8]byte
+	tBeg time.Time
+}
+
 func handleReq(ln net.Listener, tcpCh <-chan int, converter *protocol.SimpleConverter) {
 	defer ln.Close()
 
@@ -112,166 +118,168 @@ func handleReq(ln net.Listener, tcpCh <-chan int, converter *protocol.SimpleConv
 			cRespMsg := make(chan []byte, 10)
 			pRespMsg := make(chan []byte, 10)
 			elapsedCh := make(chan int64, 10)
-			go func(cConn net.Conn, cReqMsg chan<- []byte) {
-				for {
-					bl := make([]byte, 4)
-					io.ReadFull(cConn, bl)
-					lens := binary.BigEndian.Uint32(bl)
+			go clientRead(cConn, cReqMsg)
 
-					cbreq := make([]byte, lens)
-					io.ReadFull(cConn, cbreq)
-
-					log.Println("msg to cReqMsg", cbreq)
-					cReqMsg <- cbreq
-				}
-			}(cConn, cReqMsg)
-
-			// maintaing timing map
-			type TMapEntry struct {
-				id   [8]byte
-				tBeg time.Time
-			}
-
-			addCh := make(chan TMapEntry, 5)
+			addCh := make(chan tMapEntry, 5)
 			delCh := make(chan [8]byte, 5)
 			getReqCh := make(chan [8]byte, 1)
 			getRetCh := make(chan time.Time, 1)
-			go func(addCh <-chan TMapEntry, delCh, getReqCh <-chan [8]byte, getRetCh chan<- time.Time) {
-				tBegs := make(map[[8]byte]time.Time)
-				for {
-					select {
-					case entry := <-addCh:
-						tBegs[entry.id] = entry.tBeg
-					case id := <-delCh:
-						delete(tBegs, id)
-					case id := <-getReqCh:
-						getRetCh <- tBegs[id]
-					}
-				}
-			}(addCh, delCh, getReqCh, getRetCh)
+			go convertRequest(addCh, delCh, getReqCh, getRetCh)
 
 			// to provider converter
-			go func(converter *protocol.SimpleConverter, cReqMsg <-chan []byte, pReqMsg chan<- []byte, addCh chan<- TMapEntry) {
-				var cpreq protocol.CustRequest
-				for {
-					msg := <-cReqMsg
-					log.Println("msg from cReqMsg", msg)
-					cpreq.FromByteArr(msg)
-
-					timingBeg := time.Now()
-
-					dpreq, err := converter.CustomToDubbo(cpreq)
-					if err != nil {
-						log.Fatal(err)
-					}
-					dbreq, err := dpreq.ToByteArr()
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					var entry TMapEntry
-					entry.tBeg = timingBeg
-					copy(entry.id[:], dbreq[4:12])
-					addCh <- entry
-
-					pReqMsg <- dbreq
-				}
-			}(converter, cReqMsg, pReqMsg, addCh)
+			go tpConvert(converter, cReqMsg, pReqMsg, addCh)
 
 			// to server write
-			go func(pConn net.Conn, pReqMsg <-chan []byte) {
-				for {
-					dbReq := <-pReqMsg
-
-					n, err := pConn.Write(dbReq)
-
-					if err != nil || n != len(dbReq) {
-						log.Println(err)
-						return
-					}
-					//log.Println("to provider")
-					//log.Println(dbreq)
-				}
-			}(pConn, pReqMsg)
+			go providerWrite(pConn, pReqMsg)
 
 			// from server read
-			go func(pConn net.Conn, pRespMsg chan<- []byte, getReqCh, delCh chan<- [8]byte, getRetCh <-chan time.Time, elapsedCh chan int64) {
-				for {
-					dbh := make([]byte, 16)
-					_, err := io.ReadFull(pConn, dbh)
-					if err != nil {
-						log.Println(err)
-						return
-					}
-					//log.Println("Dubbo Head:", dbh)
-					lens := binary.BigEndian.Uint32(dbh[12:16])
-					dbrep := make([]byte, lens)
-					io.ReadFull(pConn, dbrep)
-					dbrep = append(dbh, dbrep...)
-
-					var id [8]byte
-					copy(id[:], dbh[4:12])
-					getReqCh <- id
-					timingBeg := <-getRetCh
-					delCh <- id
-
-					timingEnd := time.Now()
-					elapsed := timingEnd.Sub(timingBeg).Nanoseconds() / 1000
-
-					elapsedCh <- elapsed
-					pRespMsg <- dbrep
-				}
-			}(pConn, pRespMsg, getReqCh, delCh, getRetCh, elapsedCh)
+			go providerRead(pConn, pRespMsg, getReqCh, delCh, getRetCh, elapsedCh)
 
 			// from provider converter
-			go func(converter *protocol.SimpleConverter, pRespMsg <-chan []byte, cRespMsg chan<- []byte, elapsedCh <-chan int64) {
-				var dprep protocol.DubboPacks
-				for {
-					//log.Println("From provider:")
-					//log.Println(dbrep)
-					elapsed := <-elapsedCh
-					dprep.FromByteArr(<-pRespMsg)
-					cprep, err := converter.DubboToCustom(uint64(elapsed), dprep)
-					if err != nil {
-						log.Fatal(err)
-						return
-					}
-					cbrep, err := cprep.ToByteArr()
-					if err != nil {
-						log.Fatal(err)
-						return
-					}
-
-					cRespMsg <- cbrep
-				}
-			}(converter, pRespMsg, cRespMsg, elapsedCh)
+			go tcConvert(converter, pRespMsg, cRespMsg, elapsedCh)
 
 			// to client write
-			go func(cConn net.Conn, cRespMsg <-chan []byte) {
-				for {
-					bl := make([]byte, 4)
-					cbrep := <-cRespMsg
-					binary.BigEndian.PutUint32(bl, uint32(len(cbrep)))
-
-					_, err = cConn.Write(bl)
-					if err != nil {
-						log.Println(err)
-						return
-					}
-
-					//log.Println("To customer")
-					//log.Println(cbrep)
-					_, err = cConn.Write(cbrep)
-					if err != nil {
-						log.Println(err)
-						return
-					}
-				}
-			}(cConn, cRespMsg)
+			go clientWrite(cConn, cRespMsg)
 		}
 	}(converter)
 
 	<-tcpCh
+}
+
+func clientRead(cConn net.Conn, cReqMsg chan<- []byte) {
+	for {
+		bl := make([]byte, 4)
+		io.ReadFull(cConn, bl)
+		lens := binary.BigEndian.Uint32(bl)
+
+		cbreq := make([]byte, lens)
+		io.ReadFull(cConn, cbreq)
+
+		log.Println("msg to cReqMsg", cbreq)
+		cReqMsg <- cbreq
+	}
+}
+func convertRequest(addCh <-chan tMapEntry, delCh, getReqCh <-chan [8]byte, getRetCh chan<- time.Time) {
+	tBegs := make(map[[8]byte]time.Time)
+	for {
+		select {
+		case entry := <-addCh:
+			tBegs[entry.id] = entry.tBeg
+		case id := <-delCh:
+			delete(tBegs, id)
+		case id := <-getReqCh:
+			getRetCh <- tBegs[id]
+		}
+	}
+}
+func tpConvert(converter *protocol.SimpleConverter, cReqMsg <-chan []byte, pReqMsg chan<- []byte, addCh chan<- tMapEntry) {
+	var cpreq protocol.CustRequest
+	for {
+		msg := <-cReqMsg
+		log.Println("msg from cReqMsg", msg)
+		cpreq.FromByteArr(msg)
+
+		timingBeg := time.Now()
+
+		dpreq, err := converter.CustomToDubbo(cpreq)
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbreq, err := dpreq.ToByteArr()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var entry tMapEntry
+		entry.tBeg = timingBeg
+		copy(entry.id[:], dbreq[4:12])
+		addCh <- entry
+
+		pReqMsg <- dbreq
+	}
+}
+func providerWrite(pConn net.Conn, pReqMsg <-chan []byte) {
+	for {
+		dbReq := <-pReqMsg
+
+		n, err := pConn.Write(dbReq)
+
+		if err != nil || n != len(dbReq) {
+			log.Println(err)
+			return
+		}
+		//log.Println("to provider")
+		//log.Println(dbreq)
+	}
+}
+func providerRead(pConn net.Conn, pRespMsg chan<- []byte, getReqCh, delCh chan<- [8]byte, getRetCh <-chan time.Time, elapsedCh chan int64) {
+	for {
+		dbh := make([]byte, 16)
+		_, err := io.ReadFull(pConn, dbh)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		//log.Println("Dubbo Head:", dbh)
+		lens := binary.BigEndian.Uint32(dbh[12:16])
+		dbrep := make([]byte, lens)
+		io.ReadFull(pConn, dbrep)
+		dbrep = append(dbh, dbrep...)
+
+		var id [8]byte
+		copy(id[:], dbh[4:12])
+		getReqCh <- id
+		timingBeg := <-getRetCh
+		delCh <- id
+
+		timingEnd := time.Now()
+		elapsed := timingEnd.Sub(timingBeg).Nanoseconds() / 1000
+
+		elapsedCh <- elapsed
+		pRespMsg <- dbrep
+	}
+}
+func tcConvert(converter *protocol.SimpleConverter, pRespMsg <-chan []byte, cRespMsg chan<- []byte, elapsedCh <-chan int64) {
+	var dprep protocol.DubboPacks
+	for {
+		//log.Println("From provider:")
+		//log.Println(dbrep)
+		elapsed := <-elapsedCh
+		dprep.FromByteArr(<-pRespMsg)
+		cprep, err := converter.DubboToCustom(uint64(elapsed), dprep)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		cbrep, err := cprep.ToByteArr()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		cRespMsg <- cbrep
+	}
+}
+func clientWrite(cConn net.Conn, cRespMsg <-chan []byte) {
+	for {
+		bl := make([]byte, 4)
+		cbrep := <-cRespMsg
+		binary.BigEndian.PutUint32(bl, uint32(len(cbrep)))
+
+		_, err := cConn.Write(bl)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		//log.Println("To customer")
+		//log.Println(cbrep)
+		_, err = cConn.Write(cbrep)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
 
 //Stop can be used for closing provider manually.
