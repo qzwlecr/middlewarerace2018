1ed160b265242037c93a4bc0bdf6a58a689b937a
qzwlecr <qzwlecr@gmail.com>
2018-06-10 22:12:32 +0800

    Add random decreaseTime.

--- src/consumer/interface.go
+++ src/consumer/interface.go
@@ -5,16 +5,16 @@ import (
 )
 
 const (
-	logger       = true
-	dialTimeout  = 5 * time.Second
-	checkTimeout = 2 * time.Second
-	queueSize    = 1024
-	connsSize    = 256
-	oldWeight    = 0
-	newWeight    = 10 - oldWeight
-	baseTime     = 50000
-	decreaseTimeout = 50 * time.Millisecond
-	decreaseTime = 1000 * 1000
+	logger          = true
+	dialTimeout     = 5 * time.Second
+	checkTimeout    = 2 * time.Second
+	queueSize       = 1024
+	connsSize       = 256
+	oldWeight       = 0
+	newWeight       = 10 - oldWeight
+	baseTime        = 50000
+	decreaseTimeout = 200 * time.Millisecond
+	decreaseTime    = 1000 * 1000
 )
 
 type ProviderInfo struct {
