bd10383ff7a9a89445c92d66ebd6e235565504f7
qzwlecr <qzwlecr@gmail.com>
2018-06-16 22:53:07 +0800

    tuning para.

--- src/provider/provider.go
+++ src/provider/provider.go
@@ -181,7 +181,7 @@ func (p *Provider) handleReq(ln net.Listener, tcpCh <-chan int, converter *proto
 }
 
 func clientRead(cConn net.Conn, cReqMsg chan<- []byte) {
-	log.Println("Connection Accepted from consumer..")
+	//log.Println("Connection Accepted from consumer..")
 	defer cConn.Close()
 	for {
 		tm := time.Now()
@@ -200,7 +200,7 @@ func clientRead(cConn net.Conn, cReqMsg chan<- []byte) {
 			log.Println("failed to read content", err)
 			return
 		}
-		log.Println("Readed a new request from consumer..")
+		//log.Println("Readed a new request from consumer..")
 
 		// the mini-pressurer stuff
 		tmagic := binary.BigEndian.Uint64(cbreq[8:16])
@@ -264,7 +264,7 @@ func tpConvert(converter *protocol.SimpleConverter, cReqMsg <-chan []byte, pReqM
 	for {
 		tm := time.Now()
 		msg := <-cReqMsg
-		log.Println("source req accepted.")
+		//log.Println("source req accepted.")
 		//log.Println("msg from cReqMsg", msg)
 		cpreq.FromByteArr(msg)
 		dpreq, err := converter.CustomToDubbo(cpreq)
@@ -288,7 +288,7 @@ func providerWrite(pConn net.Conn, pReqMsg <-chan []byte) {
 	for {
 		tm := time.Now()
 		dbReq := <-pReqMsg
-		log.Println("dubbo accepted.")
+		//log.Println("dubbo accepted.")
 		//log.Println("out", dbReq)
 		n, err := pConn.Write(dbReq)
 
@@ -318,7 +318,7 @@ func providerRead(pConn net.Conn, pRespMsg chan<- []byte) {
 			log.Println(err)
 			return
 		}
-		log.Println("dubbo reply read..")
+		//log.Println("dubbo reply read..")
 		dbrep = append(dbh, dbrep...)
 
 		var id [8]byte
@@ -334,7 +334,7 @@ func tcConvert(converter *protocol.SimpleConverter, pRespMsg <-chan []byte, cRes
 		//log.Println("From provider:")
 		//log.Println(dbrep)
 		// dprep.FromByteArr(<-pRespMsg)
-		log.Println("dubb2cust cnvt recevied..")
+		//log.Println("dubb2cust cnvt recevied..")
 		msg := <-pRespMsg
 		dprep.FromByteArr(msg)
 		cprep, err := converter.DubboToCustom(uint64(0), dprep)
@@ -362,7 +362,7 @@ func clientWrite(cConn net.Conn, cRespMsg <-chan []byte) {
 		tm := time.Now()
 		bl := make([]byte, 4)
 		cbrep := <-cRespMsg
-		log.Println("needwrite to consumer received..")
+		//log.Println("needwrite to consumer received..")
 		binary.BigEndian.PutUint32(bl, uint32(len(cbrep)))
 
 		_, err := cConn.Write(bl)
